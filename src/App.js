41a5456afdd7588560f8126839591670a305d249
Mayank sharma <msharma@innow8apps.com>
2022-04-07 16:21:27 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
           <Collapse navbar>
             <Nav className="ms-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Homesaiouashydiuasbjkd</NavLink>
+                <NavLink href="/components/">Home22311231</NavLink>
               </NavItem>
               <NavItem>
                 <NavLink href="https://github.com/reactstrap/reactstrap">
